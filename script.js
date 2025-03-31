cd4b6150cbdd26403c7541a030a349ebee99ff02
Shreyashchimote <shreyashchimote08@gmail.com>
2025-03-31 20:30:04 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+const totalSlides = slides.length;
+const slider = document.querySelector('.slider');
+
+function showSlide(index) {
+  if (index >= totalSlides) {
+    currentSlide = 0;
+  } else if (index < 0) {
+    currentSlide = totalSlides - 1;
+  } else {
+    currentSlide = index;
+  }
+
+  const offset = -currentSlide * 100;
+  slider.style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide() {
+  showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+  showSlide(currentSlide - 1);
+}
+
+// Optional: Auto-slide every 5 seconds
+setInterval(nextSlide, 5000);
